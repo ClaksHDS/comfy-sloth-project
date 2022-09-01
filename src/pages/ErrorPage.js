60b309a4b32de6e86200103c61f9e4091306448b
Your Namehuetclariss <you@example.com>
2022-09-01 13:19:06 +0100

    set up error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <h4>error page</h4>
-}
+  return (
+    <Wrapper className='page-100'>
+      <section>
+        <h1>404</h1>
+        <h3>sorry, the page you tried cannot be found</h3>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
+      </section>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.main`
   background: var(--clr-primary-10);
@@ -12,12 +23,12 @@ const Wrapper = styled.main`
   align-items: center;
   text-align: center;
   h1 {
-    font-size: 10rem;
+    font-size: 8rem;
   }
   h3 {
     text-transform: none;
     margin-bottom: 2rem;
   }
-`
+`;
 
-export default ErrorPage
+export default ErrorPage;
