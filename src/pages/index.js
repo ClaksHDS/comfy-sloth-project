2881c6f42eabae79856c3af640338ffc582abd0e
Your Namehuetclariss <you@example.com>
2022-09-01 10:03:34 +0100

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  About,
+  Cart,
+  Products,
+  SingleProduct,
+  Checkout,
+  Error,
+  PrivateRoute,
+};
