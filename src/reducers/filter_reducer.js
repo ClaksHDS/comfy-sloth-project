3cd90585d1c955e873f04a734269f2ccff8b9a6b
Your Namehuetclariss <you@example.com>
2022-09-05 11:25:08 +0100

    Filters Functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -62,8 +62,41 @@ const filter_reducer = (state, action) => {
     return { ...state, filters: { ...state.filters, [name]: value } };
   }
   if (action.type === FILTER_PRODUCTS) {
-    console.log("filtering products");
-    return { ...state };
+    const { all_products } = state;
+    const { text, company, category, colors, price, shipping } = state.filters;
+    let tempProducts = [...all_products];
+
+    // filtering
+    if (text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.name.toLowerCase().startsWith(text);
+      });
+    }
+    if (category !== "All") {
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      );
+    }
+    if (company !== "All") {
+      tempProducts = tempProducts.filter(
+        (product) => product.company === company
+      );
+    }
+    if (colors !== "All") {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === colors);
+      });
+    }
+    // price
+    tempProducts = tempProducts.filter((product) => product.price <= price);
+
+    if (shipping) {
+      tempProducts = tempProducts.filter(
+        (product) => product.shipping === true
+      );
+    }
+
+    return { ...state, filtered_products: tempProducts };
   }
 
   if (action.type === CLEAR_FILTERS) {
