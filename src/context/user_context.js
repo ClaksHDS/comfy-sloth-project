9c63b11ba05b26571d3f9f97abe2eb1c7e010d03
Your Namehuetclariss <you@example.com>
2022-09-06 10:04:00 +0100

    Fix warnings in console

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -2,19 +2,16 @@ import React, { useContext, useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const UserContext = React.createContext();
+
 export const UserProvider = ({ children }) => {
-  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } =
-    useAuth0();
+  const { user, loginWithRedirect, logout } = useAuth0();
 
   const [myUser, setMyUser] = useState(null);
 
   useEffect(() => {
-    if (isAuthenticated) {
-      setMyUser(user);
-    } else {
-      setMyUser(false);
-    }
-  }, [isAuthenticated]);
+    setMyUser(user);
+  }, [user]);
+
   return (
     <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
       {children}
--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const DOMAIN = `${process.env.REACT_APP_AUTH0_DOMAIN_KEY}`;
-const CLIENT_ID = `${process.env.REACT_APP_AUTH0_CLIENT_ID_KEY}`;
+/* const DOMAIN = `${process.env.REACT_APP_AUTH0_DOMAIN_KEY}`;
+const CLIENT_ID = `${process.env.REACT_APP_AUTH0_CLIENT_ID_KEY}`; */
 
 ReactDOM.render(
   <Auth0Provider
